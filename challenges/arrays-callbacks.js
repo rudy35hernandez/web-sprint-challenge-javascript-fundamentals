deee275ee179297da5524bb4619277cb810f991b
rudy35hernandez <rudy35hernandez@gmail.com>
2020-10-11 21:16:27 -0700

    finished. Just answer test written questions

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -1,6 +1,7 @@
 // ==== ADVANCED Array Methods ====
 
-// Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
+// Given this zoo data from around the United States, follow the instructions below.  Use the specific array 
+// methods in the requests below to solve the problems.
 
 const zooAnimals = [
   { animal_name: "Jackal, asiatic", population: 5, scientific_name: "Canis aureus", state: "Kentucky" },
@@ -17,25 +18,33 @@ const zooAnimals = [
 
 /* Request 1: .forEach()
 
-The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
+The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the 
+displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of 
+strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
+
 const displayNames = [];
-const nameScientific = zooAnimals.forEach((item) => {
-  return `Name: ${animal_name}, Scientific: ${scientific_name};`;
+zooAnimals.forEach((item) => {
+ let nameScientific =`Name: ${item.animal_name}, Scientific: ${item.scientific_name};`
+{
+  return displayNames.push(nameScientific);
 }
- displayNames.push(nameScientific);
-)
+});
 
 console.log(displayNames);
 
+
+
 /* Request 2: .map()
 
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a 
+new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the
+resut.
 
 */
 
-const lowCaseAnimalNames = zooAnimals.map(function(item) { 
+const lowCaseAnimalNames = zooAnimals.map((item) => { 
   item.animal_name = item.animal_name.toLowerCase();
   return `${item.animal_name}`
 });
@@ -44,7 +53,8 @@ console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
-The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects 
+called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
 const lowPopulationAnimals = zooAnimals.filter((item) =>
@@ -55,7 +65,9 @@ console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. Find the total population from all 
+the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes
+two args), and an initial value for the count.
 
 */
 let populationTotal = 0;
@@ -76,13 +88,28 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b , cb) {
+  return cb(a, b)
+ }
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, 
+  nice to meet you!"
 */
 
+function add(addNum1, addNum2){
+  return addNum1 + addNum2
+  }
+
+function multiply(multNum1, multNum2){
+  return multNum1 * multNum2
+  }
+
+  function greeting(nameOne, nameTwo) {
+    return `Hello ${nameOne} ${nameTwo}, nice to meet you!`
+    }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -1,6 +1,7 @@
 /* ===== Prototype Practice ===== */
 
-// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
+// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are 
+// similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
 
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
@@ -14,7 +15,8 @@ function CuboidMaker(length, width, height){
 
 
 /* == Step 2: Volume Method ==
-  Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
+  Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and 
+  height
   
   Formula for cuboid volume: length * width * height
 */
@@ -26,7 +28,8 @@ CuboidMaker.prototype.volume = function(totalVolume) {
 
 
 /* == Step 3: Surface Area Method ==
-  Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
+  Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, 
+  width, and height. 
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
